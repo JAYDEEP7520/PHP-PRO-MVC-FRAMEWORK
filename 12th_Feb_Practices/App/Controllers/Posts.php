f33a9075ebf4029512f4b5ce1f6732e5cbe06e0e
JAYDEEP <jppandya1999@gmail.com>
2020-02-13 10:56:29 +0530

    Manging Code using Composer

---
+++ 12th_Feb_Practices/App/Controllers/Posts.php
@@ -0,0 +1,23 @@
+<?php
+    namespace App\Controllers;
+
+    use \Core\View;
+    
+    class Posts extends \Core\Controller
+    {
+        public function indexAction() 
+        {
+            echo "Hello from the index action in posts controller <br>";
+            View::renderTemplate('Posts/index.html');
+        }
+        public function addNewAction() 
+        {
+            echo "Hello from the addNew action in posts controller"; 
+        }
+        public function editAction() 
+        {
+            echo "Hello from the edit action in posts controller <br>";
+            echo "Route Parameters: <pre>" . htmlspecialchars(print_r($this->route_params, true)) . "</pre>";
+        }
+    }
+?>
